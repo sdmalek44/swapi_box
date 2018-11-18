40cd8793399909faf588a8fd7bd1009b53b9b217
Stephen Malek <malek.stephen44@gmail.com>
2018-11-18 15:01:07 -0700

    Add movie text component to app

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,17 +1,43 @@
 import React, { Component } from "react";
 import "./styles.css";
+import MovieText from "../MovieText";
 
 class App extends Component {
+  state = {
+    movie: {}
+  };
+
+  componentDidMount() {
+    this.getMovieData();
+  }
+
+  randomFilmUrl = () => {
+    let film = Math.floor(Math.random() * 7) + 1;
+    return `https://swapi.co/api/films/${film}/`;
+  };
+
+  getMovieData = async () => {
+    try {
+      const response = await fetch(this.randomFilmUrl());
+      const movie = await response.json();
+      this.setState({ movie });
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
+
   render() {
     return (
-      <div className="App">
-        <aside className="movieScroll" />
+      <div className="app">
+        <aside className="movie-scroll">
+          <MovieText movie={this.state.movie} />
+        </aside>
         <section className="main">
           <header className="head">
             <span className="title">SWAPI-Box</span>
-            <span className="favoritesNumber">Favorites: 0</span>
+            <button className="favorites-number">Favorites: 0</button>
           </header>
-          <article className="resultsBox" />
+          <article className="results-box" />
         </section>
       </div>
     );
